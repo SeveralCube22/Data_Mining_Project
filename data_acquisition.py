8fdca28364317d49b9543a3b2d0022f605ab7d3f
SeveralCube22 <manam.viswadeep@gmail.com>
2022-02-14 22:47:46 -0800

    Working selections

--- data_acquisition.py
+++ data_acquisition.py
@@ -5,26 +5,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
+def get_keys():
+    f = open("./sensitive.txt", "r")
+    for line in f:
+        (key, value) = line.split(",")
+        sensitive[key] = value
+        
+def login():
+    username = driver.find_element(By.XPATH, "//*[@id='email-field']")
+    username.send_keys(sensitive["USERNAME"])
+    pswd = driver.find_element(By.XPATH, "//*[@id='passwordInput']")
+    pswd.send_keys(sensitive["PASSWORD"])
+    wait_and_click("//*[@id='application']/div/div/div[3]/form/div[1]/button", click_btn)
 
 def click_btn(xpath):
     btn = driver.find_element(By.XPATH, xpath)
     driver.execute_script ("arguments[0].click();", btn)
 
 def wait_for_element(xpath):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath)))
 
-def wait_and_click(xpath):
+def wait_and_click(xpath, func, **values):
     wait_for_element(xpath)
-    click_btn(xpath)
+    value = values.get("value")
+    func(xpath, value) if value else func(xpath)
+    
+def select(xpath, value):
+    selector = Select(driver.find_element(By.XPATH, xpath))
+    selector.select_by_value(value)
     
 def select_place():
-    (place, state, cities) = "//*[@id='cmbGeoType']/option[7]", "//*[@id='geoCombosContainer']/div/select/option[6]", "//*[@id='listGeoItems']/option[2]"
-    
-    print("BEFORE")
-    wait_and_click(place) # select PLACE
-    print("AFTER")
-    wait_and_click(state) # select State(CA)
-    wait_and_click(cities) # select all places(cities) in state
+    xpath = "//*[@id='cmbGeoType']"
+    wait_and_click(xpath, select, value="SL160") # select PLACE
+    xpath = "//*[@id='geoCombosContainer']/div/select"
+    wait_and_click(xpath, select, value="06;160;California") # select State(CA)
+    xpath = "//*[@id='listGeoItems']"
+    wait_and_click(xpath, select, value="06;160; ") # select all places(cities) in state
     
     click_btn("//*[@id='btnAddGeoItem']") # click add
     click_btn("//*[@id='btnGeoNext']") # click next
@@ -32,11 +48,16 @@ def select_place():
 def select_properties():
     pass
     
-sol_explore = "https://www.socialexplorer.com/explore-tables"
+    
+sensitive = {}
+get_keys()
 
+sol_explore = "https://www.socialexplorer.com/explore-tables"
 driver = webdriver.Chrome(executable_path='C:/Users/manam/Desktop/chromedriver_win32/chromedriver.exe')
 driver.get(sol_explore)
-click_btn("//*[@id='dashboard']/div[5]/div/div/div[5]/div[1]/button")
+login()
+
+wait_and_click("//*[@id='dashboard']/div[5]/div/div/div[5]/div[1]/button", click_btn)
 
 for i in range(11):
     click_btn("//*[@id='dashboard']/div[5]/div/div/div[5]/div[2]/div[{}]/div[2]/a[1]".format(2 + i)) # iterating over begin     report buttons for years 2009-2019
@@ -47,6 +68,9 @@ for i in range(11):
     
     select_place()
     select_properties()
+    
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
     
     # parse resultant html page
     
