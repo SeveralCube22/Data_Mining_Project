2b0c2f10a315a4f3b87225a4d65ce10f0b2d61e9
SeveralCube22 <manam.viswadeep@gmail.com>
2022-02-17 18:20:40 -0800

    City properties. Bug fixing

--- city_data.py
+++ city_data.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 from sensitive import get_keys
+
+import state
         
 def get_state_data():
     state_data = {}
@@ -38,23 +40,26 @@ def wait_for_element(xpath):
 def wait_and_click(xpath, func, **values):
     wait_for_element(xpath)
     value = values.get("value")
+    
     func(xpath, value) if value else func(xpath)
     
 def select(xpath, value):
     selector = Select(driver.find_element(By.XPATH, xpath))
     selector.select_by_value(value)
     
-def select_place(stateNum=None, stateName=None):
+def select_place(stateName=None, stateNum=None):
     xpath = "//*[@id='cmbGeoType']"
     wait_and_click(xpath, select, value="SL160") # select PLACE
     xpath = "//*[@id='geoCombosContainer']/div/select"
     wait_for_element(xpath)
     
-    if(stateNum == None):
+    if(stateName == None):
         global states
         states = get_state_data() # have to do this because HTML attributes doesn't show initially
     else:
+        print("{};160;{}".format(stateNum, stateName))
         wait_and_click(xpath, select, value="{};160;{}".format(stateNum, stateName)) # select state
+        print("HERE")
         
         xpath = "//*[@id='listGeoItems']"
         wait_and_click(xpath, select, value="{};160; ".format(stateNum)) # select all places(cities) in state
@@ -63,25 +68,48 @@ def select_place(stateNum=None, stateName=None):
         click_btn("//*[@id='btnGeoNext']") # click next
     
 def select_properties():
-    pass
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    properties = [i for i in soup.find("select", {"id": "tableCombo"})]
+    select_xpath = "//*[@id='tableCombo']"
+    wait_for_element(select_xpath)
+    
+    print("PROPERTIES OPTIONS")
+    
+    for prop in city_properties:
+        for super in properties: # list of options
+            if is_subset(super.text, prop):
+                print(super.text, prop)
+                wait_and_click(select_xpath, select, value=super["value"])
+                click_btn("//*[@id='btnAddTable']") # click add
+                break
+            
+    click_btn("//*[@id='btnTableNext']") # click next
+
+def is_subset(super, sub):
+    super_list = super.split(" ")
+    sub_list = sub.split(" ")
+    return super_list.issubset(sub_list)
     
 
 def get_city_data():
     wait_and_click("//*[@id='dashboard']/div[5]/div/div/div[5]/div[1]/button", click_btn)
-
+    init = False
+    
     for i in range(11):
         click_btn("//*[@id='dashboard']/div[5]/div/div/div[5]/div[2]/div[{}]/div[2]/a[1]".format(2 + i)) # iterating over begin     report buttons for years 2009-2019
    
         WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))   
         child = driver.window_handles[1]      
-        driver.switch_to.window(child) 
-        select_place()
+        driver.switch_to.window(child)
+        if not init: 
+            select_place()
+            init = True
         
         for (key, value) in states.items():
-            if(scrape_states.get(key)):
+            if(key in state.scrape_states.keys()):
                 select_place(key, value) # pass state(other terrorties included) num 1-72 and state name
                 select_properties()
-    
+
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
     
@@ -92,7 +120,8 @@ def get_city_data():
      
 sensitive = get_keys()
 states = {} # parse HTML to get stateNums and stateNames for use in select_place function
-scrape_states = {"California": None, "Alabama": None, "New York": None, "Texas": None}
+
+city_properties = ["Total Population", "Population Density", "Land Area", "Sex by Age", "Race", "Households by Household Type", "Household Size (Renter-Occupied Housing Units", "Educational Attainment for Population 25 Years and Over", "School Dropout Rate for Population 16 to 19 Years", "Employment Status for Total Population", "Unemployment Rate for Civilian Population", "Industry by Occupation for Employed Civilian Population", "Occupation for Employed Civilian Population", "Average Household Income", "Average Household Income by Race", "Gini Index", "Housing Units by Monthly Housing Costs", "Poverty Status for Population Age 18 to 64", "Poverty Status for Population Age 65 and Over", "Means of Transportation to Work for Workers"]
 
 sol_explore = "https://www.socialexplorer.com/explore-tables"
 driver = webdriver.Chrome(executable_path='C:/Users/manam/Desktop/chromedriver_win32/chromedriver.exe')
---
+++ state.py
@@ -0,0 +1,4 @@
+def get_ori_code(state):
+    pass
+
+scrape_states = {"California": None, "Alabama": None, "New York": None, "Texas": None}
